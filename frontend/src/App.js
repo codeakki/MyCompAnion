57bc1d16ccae730f5cf7c96be71c856a8d3e58f7
Akshay Sharma <akshaysharma@Akshays-MacBook-Air.local>
2022-08-18 12:52:14 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,16 @@ import Billing from "./pages/Billing";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import Calendar from "./pages/Calendar";
+import Calendar from "./pages/Calendar"
 import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
-import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={LandingPage} />
-
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" exact component={SignIn} />
         <Main>
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,16 +1,4 @@
-/*!
-  =========================================================
-  * Muse Ant Design Dashboard - v1.0.0
-  =========================================================
-  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-  * Coded by Creative Tim
-  =========================================================
-  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import { useState } from "react";
-
 import {
   Row,
   Col,
@@ -24,14 +12,12 @@ import {
   Upload,
   message,
 } from "antd";
-
 import {
   FacebookOutlined,
   TwitterOutlined,
   InstagramOutlined,
   VerticalAlignTopOutlined,
 } from "@ant-design/icons";
-
 import BgProfile from "../assets/images/bg-profile.jpg";
 import profilavatar from "../assets/images/face-1.jpg";
 import convesionImg from "../assets/images/face-3.jpg";
